314c8ebb2cc4ab180665bd453141be29b350bace
Stephanie Savoie <stephsavoie4@gmail.com>
2017-11-06 15:56:16 -0500

    added validation to artwork model

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,2 +1,7 @@
 class Artwork < ApplicationRecord
+
+  validates :name, presence: true
+  validates :artist, presence: true
+  validates :medium, presence: true
+  validates :size, presence: true
 end
