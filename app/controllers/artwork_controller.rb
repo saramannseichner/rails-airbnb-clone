429ee293a8005fe733c31c19a876f2b01cb472e0
Jesse Hammond <jessethammond@gmail.com>
2017-11-06 16:37:03 -0500

    added edit, update, create, new controller methods

--- app/controllers/artwork_controller.rb
+++ app/controllers/artwork_controller.rb
@@ -8,15 +8,30 @@ class ArtworkController < ApplicationController
   end
 
   def new
+    @artwork = Artwork.new
+    @user = User.find(params[:id])
   end
 
   def create
+    @artwork = Artwork.new(artwork_params)
+    if @artwork.save!
+      redirect_to artwork_path(@artwork)
+    else
+      render :new
+    end
   end
 
   def edit
+    @artwork = Artwork.find(params[:id])
   end
 
   def update
+    @artwork = Artwork.new(artwork_params)
+     if @artwork.save!
+      redirect_to artwork_path(@artwork)
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -24,5 +39,7 @@ class ArtworkController < ApplicationController
 
   private
 
-
+  def artwork_params
+    params.require(:artwork).permit(:name, :medium, :size, :description, :artist, :address)
+  end
 end
