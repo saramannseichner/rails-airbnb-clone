f6f64f765b1394d09a727024c5461c2d01a8b1c2
Stephanie Savoie <stephsavoie4@gmail.com>
2017-11-06 16:04:48 -0500

    added validation to reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,8 @@
 class Reservation < ApplicationRecord
   belongs_to :artwork
   belongs_to :user
+
+  validates :artwork, presence: true
+  validates :user, presence: true
+  validates :date, presence: true
 end
