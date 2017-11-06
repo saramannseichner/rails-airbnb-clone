0eab9131a1d001133442c08a3a7ccbca4f2a9f5d
Stephanie Savoie <stephsavoie4@gmail.com>
2017-11-06 15:52:46 -0500

    added has_many artworks reference to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :reservations
+  has_many :artworks, through: :reservations
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
