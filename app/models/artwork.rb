7b3216b756cc2f5242d03fe21610118d1cff73b8
Stephanie Savoie <stephsavoie4@gmail.com>
2017-11-07 10:38:01 -0500

    added photo to user and artwork model and controller

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,6 +1,7 @@
 class Artwork < ApplicationRecord
   has_many :reservations
   has_many :users, through: :users
+  has_attachment :photo
 
   validates :name, presence: true
   validates :artist, presence: true
