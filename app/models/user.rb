f9c161515df4c2b6444ed832f5c15158ca073457
Sara Mannseichner <saramannseichner@gmail.com>
2017-11-07 10:43:43 -0500

    devise added to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, omniauth_providers: [:facebook]
 
   validates :username, presence: true, uniqueness: true
 end
