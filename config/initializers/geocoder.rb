755963aa04fad37bccb981e6a041d90c86127217
Sara Mannseichner <saramannseichner@gmail.com>
2017-11-08 11:42:28 -0500

    sent api key to heroku

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -20,3 +20,10 @@ Geocoder.configure(
   units: :km,                 # :km for kilometers or :mi for miles
   # distances: :linear          # :spherical or :linear
 )
+
+Geocoder.configure(
+  :lookup    => :google,
+  :api_key   => ENV['GOOGLE_API_SERVER_KEY'],
+  :use_https => true,
+  # [...]
+)
