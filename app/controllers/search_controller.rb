647faa1a63aedb8f881be6f56648e1ab282db605
Sara Mannseichner <saramannseichner@gmail.com>
2017-11-08 15:40:07 -0500

    Deleted jesse's controller

--- app/controllers/search_controller.rb
+++
@@ -1,9 +0,0 @@
-class SearchController < ApplicationController
-  skip_before_action :authenticate_user!
-
-  if params[:search]
-    @artworks = Artwork.search(params[:search]).order("created_at DESC")
-  else
-    @artworks = Artwork.all.order('created_at DESC')
-  end
-end
