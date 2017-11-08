2337952666e83971abcd8ac58ed14cab365e7163
Jesse Hammond <jessethammond@gmail.com>
2017-11-08 13:52:20 -0500

    Changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   resources :users, only: [ :index ] do
-      resources :artworks, only: [ :update, :edit, :destroy ]
+      resources :artworks, only: [ :update, :edit, :destroy
+      resources :reservations, only: [ :show, :index ]
   end
-  resources :artworks, only: [ :index, :show, :new, :create]
+
+  resources :artworks, only: [ :index, :show, :new, :create] do
+    resources :reservations, only: [ :new, :create ]
+  end
+
+  resources :reservations, only: [ :update, :edit, :destroy ]
+
   resources :search, only: [ :index ]
 
   mount Attachinary::Engine => "/attachinary"
