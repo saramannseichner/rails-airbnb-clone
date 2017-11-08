3505e7237b012d65630c6461f5fb0b24bd9c3157
Sara Mannseichner <saramannseichner@gmail.com>
2017-11-08 10:56:50 -0500

    added coordinates to artwork and db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107153859) do
+ActiveRecord::Schema.define(version: 20171108155602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20171107153859) do
     t.string "artist"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "attachinary_files", id: :serial, force: :cascade do |t|
