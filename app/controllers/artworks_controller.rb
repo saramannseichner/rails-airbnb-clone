ab6bd64cf9b4ceb1346f490ecec6d91313ba9cf0
Sara Mannseichner <saramannseichner@gmail.com>
2017-11-08 11:36:20 -0500

    added to artworks index to show markers

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -3,9 +3,17 @@ class ArtworksController < ApplicationController
 
   def index
     @artworks = Artwork.all
+
+    @artworks = Artwork.where.not(latitude: nil, longitude: nil)
+
+    @hash = Gmaps4rails.build_markers(@artworks) do |artwork, marker|
+      marker.lat artwork.latitude
+      marker.lng artwork.longitude
   end
 
   def show
+    @artworks = Artwork.find(params[:id])
+    @artwork_coordinates = { lat: @flat.latitude, lng: @flat.longitude }
   end
 
   def new
@@ -47,4 +55,5 @@ class ArtworksController < ApplicationController
   def artwork_params
     params.require(:artwork).permit(:name, :medium, :size, :description, :artist, :address, :photo)
   end
+
 end
